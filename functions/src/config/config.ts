6da24dec9b9933a925c86610445825583384892b
Sayan Dey <sayandey450@gmail.com>
2024-08-21 15:18:36 +0530

    add sample config

---
+++ functions/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  "smtpConfig": {
+    host: "xxx.xxx.xxx.xxx",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "xxx@xxx.xxx",
+      pass: "XXXXXXX",
+    },
+  },
+  "destinations": [
+    "xxxxxxx@xxx.xxx",
+  ],
+};
